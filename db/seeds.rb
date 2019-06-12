23d87b0f9a26088d49fa0feaa04778c78cfc2c1f
AnuyaPadte <anuya.padte@somaiya.edu>
2019-06-12 19:17:01 +0530

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ User.create!(name:  "Nanu",
              email: "nanu@gmail.com",
              password:              "anuya17",
              password_confirmation: "anuya17",
-             admin:     true,
+             admin:     false,
              activated: true,
              activated_at: Time.zone.now)
 
@@ -30,4 +30,10 @@ User.create!(name:  "Nanu",
                email: email,
                password:              password,
                password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
